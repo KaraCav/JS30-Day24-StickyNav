4dbc10b7ca41e3d860c72710df738128271900c8
Kara Cavanaugh <karacav@msn.com>
2018-03-18 21:43:15 -0400

    finished

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+//functions
+function makeSticky() {
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add('fixed-nav');
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+  } else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
+}
+//event listeners
+window.addEventListener('scroll', makeSticky);
